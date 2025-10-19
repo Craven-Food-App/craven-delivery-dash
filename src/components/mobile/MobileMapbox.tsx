72bd5613a5c4b442e5e5ea5d9b22777726f96cba
ApexRecording <guaproyal@gmail.com>
2025-10-19 12:57:07 -0400

    Move recenter button up another 50px
    Final positioning adjustment for recenter button:
    
    Changes:
    - Changed from bottom-[276px] to bottom-[326px]
    - Moved up by 50px as requested
    - Total distance from bottom: 326px
    - Now positioned higher for optimal visibility
    
    The recenter button is now at its final position,
    326px from the bottom of the screen.

--- src/components/mobile/MobileMapbox.tsx
+++ src/components/mobile/MobileMapbox.tsx
@@ -261,7 +261,7 @@ export const MobileMapbox: React.FC<MobileMapboxProps> = ({
       </div>
 
       {/* Map Controls: Recenter button - DoorDash style bottom right */}
-      <div className="absolute bottom-[276px] right-4 z-30 pointer-events-auto">
+      <div className="absolute bottom-[326px] right-4 z-30 pointer-events-auto">
         <button 
           onClick={() => {
             if (location && map.current) {
